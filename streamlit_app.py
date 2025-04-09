d56039940b64bbeedf077058119ecd9d25b5ad28
jarvid488 <32535216+jarvid488@users.noreply.github.com>
2025-04-09 18:30:07 +0530

    Update streamlit_app.py
    introduced the pandas data frame and testing it

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,17 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 #option = st.selectbox(
 #    "What is your favorite fruit ?",
 #    ("Banana", "strawberries", "Peaches"),
 #)
 
 #st.write("You selected:", option)
-
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 ingredients_list=st.multiselect(
     'choose up to 5 ingredients',
      my_dataframe,
