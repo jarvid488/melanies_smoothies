fcf2afc10b4bf2f8c9178a807d28f04ab59fc39a
jarvid488 <32535216+jarvid488@users.noreply.github.com>
2025-04-09 11:34:01 +0530

    Update streamlit_app.py
    Committing the API Data Frame to the changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if ingredients_list:
 #editable_df = st.data_editor(my_dataframe1)
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
